d330a512261c2f4bd5b90ade55dc5578c661f194
Alex Ng <alexkng97@gmail.com>
2021-02-07 23:28:29 +0000

    day13 part one

---
+++ AdventOfCode/src/com/sparta/Day13.java
@@ -0,0 +1,69 @@
+package com.sparta;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class Day13 {
+	private int earliestTimestamp;
+	private ArrayList<Integer> busID;
+	private int timeOfEarliestBus;
+	private int idOfEarliestBus;
+
+	public Day13() {
+		busID = new ArrayList<>();
+	}
+
+	public static ArrayList<String> loadFile(String url){
+		ArrayList<String> results = new ArrayList<>();
+		try {
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(url));
+			String line;
+			while((line = bufferedReader.readLine()) != null){
+				results.add(line);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+		return results;
+	}
+
+	public void processLines(ArrayList<String> lines){
+		earliestTimestamp = Integer.parseInt(lines.get(0));
+		String[] secondLine = lines.get(1).split(",");
+		for(String buses : secondLine){
+			if(!buses.equals("x")){
+				busID.add(Integer.parseInt(buses));
+			}
+		}
+	}
+
+	public void getEarliestBus(){
+		boolean foundTime = false;
+		int time = earliestTimestamp;
+		do{
+			for(int id: busID){
+				if(time % id == 0){
+					timeOfEarliestBus = time;
+					idOfEarliestBus = id;
+					foundTime = true;
+				}
+				time++;
+			}
+		}while(!foundTime);
+	}
+
+	public int getPartOneAnswer(){
+		int wait = timeOfEarliestBus - earliestTimestamp;
+		return idOfEarliestBus * wait;
+	}
+
+
+	public static void main(String[] args) {
+		Day13 day13 = new Day13();
+		day13.processLines(Day13.loadFile("AdventOfCode/day13input.txt"));
+		day13.getEarliestBus();
+		System.out.println(day13.getPartOneAnswer());
+	}
+}
