9eae62d87d42dea0b0f7c9e246ac792e2e3d142b
Alex Ng <alexkng97@gmail.com>
2021-02-11 22:44:08 +0000

    part two attempted

--- AdventOfCode/src/com/sparta/Day13.java
+++ AdventOfCode/src/com/sparta/Day13.java
@@ -59,11 +59,58 @@ public class Day13 {
 		return idOfEarliestBus * wait;
 	}
 
+	public static String[] splitString(String line){
+		return line.split(",");
+	}
+
+	public static long partTwo(String[] buses){
+		long timestamp = 100000000000000L;
+		int firstBus = Integer.parseInt(buses[0]);
+		boolean found = false;
+		do{
+			timestamp++;
+			if(timestamp % firstBus == 0){
+				found = isTimestampValid(timestamp, buses);
+
+			}
+
+		}while(!found);
+
+		return timestamp;
+
+	}
+
+	public static boolean isTimestampValid(long timestamp, String[] buses){
+		//System.out.println(timestamp);
+		for(int i = 1; i < buses.length; i++){
+			timestamp++;
+			if(!buses[i].equals("x")){
+				if(timestamp % Integer.parseInt(buses[i]) != 0){
+					return false;
+				}
+
+			}
+		}
+		return true;
+
+	}
+
 
 	public static void main(String[] args) {
 		Day13 day13 = new Day13();
 		day13.processLines(Day13.loadFile("AdventOfCode/day13input.txt"));
 		day13.getEarliestBus();
 		System.out.println(day13.getPartOneAnswer());
+
+
+//		System.out.println(Day13.partTwo(Day13.splitString("7,13,x,x,59,x,31,19")));
+//		System.out.println(Day13.partTwo(Day13.splitString("17,x,13,19")));
+//		System.out.println(Day13.partTwo(Day13.splitString("67,7,59,61")));
+
+		ArrayList<String> lines = Day13.loadFile("AdventOfCode/day13input.txt");
+		//System.out.println(lines.get(1));
+		System.out.println(Day13.partTwo(Day13.splitString(lines.get(1))));
+
+
 	}
 }
