b36b2612f34d877755ef86be81cf8a37b9d8cacd
Alex Ng <alexkng97@gmail.com>
2021-02-22 22:09:32 +0000

    14 started

--- AdventOfCode/src/com/sparta/Day14.java
+++ AdventOfCode/src/com/sparta/Day14.java
@@ -2,12 +2,15 @@ package com.sparta;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Day14 {
 	HashMap<Integer,String> mask;
+	HashMap<String, Integer> memory;
 
 	public Day14(){
 		mask = new HashMap<>();
+		memory = new HashMap<>();
 	}
 
 	public static String decimalToBinary(int decimal){
@@ -21,12 +24,26 @@ public class Day14 {
 	public void setMask(String firstLine){
 		firstLine = firstLine.substring(firstLine.indexOf('=') + 1).trim();
 		System.out.println(firstLine);
+
 		for(int i = 0; i < firstLine.length(); i++){
 			if(firstLine.charAt(i) != 'X'){
 				mask.put(i,String.valueOf(firstLine.charAt(i)));
 			}
 		}
 
+	}
+
+	public static String padBinary(String binary){
+		return String.format("%036d", Integer.parseInt(binary));
+	}
+
+	public String maskOverwrite(String binary){
+		StringBuilder result = new StringBuilder(binary);
+		for(Map.Entry<Integer, String> entry: mask.entrySet()){
+			result.setCharAt(entry.getKey(),entry.getValue().charAt(0));
+		}
+
+		return result.toString();
 	}
 
 
